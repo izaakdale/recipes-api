84524e8f43ad6d9bfade44b1415d374fb038ad7f
Izaak Dale <izaakdaledev@gmail.com>
2023-06-04 16:24:09 +0100

    recipe data model

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	gin.Default().Run()
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"published_at"`
 }
