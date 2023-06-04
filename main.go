23831a3b2e8acb3473c45858e70da3e9617f294a
Izaak Dale <izaakdaledev@gmail.com>
2023-06-04 16:51:36 +0100

    fetch endpoint and mock data init

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	"github.com/google/uuid"
 )
 
 var recipes []Recipe
 
 func init() {
 	recipes = make([]Recipe, 0)
+	file, err := os.ReadFile("recipes.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(file), &recipes)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
 }
 
 func NewRecipeHandler(c *gin.Context) {
@@ -28,7 +43,7 @@ func NewRecipeHandler(c *gin.Context) {
 		})
 		return
 	}
-	recipe.ID = xid.New().String()
+	recipe.ID = uuid.NewString()
 	recipe.PublishedAt = time.Now()
 	recipes = append(recipes, recipe)
 	c.JSON(http.StatusOK, recipe)
