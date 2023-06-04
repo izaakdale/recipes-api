b964c14491d9e533ac54e51af12ffdd2b6a36c4b
Izaak Dale <izaakdaledev@gmail.com>
2023-06-04 17:07:47 +0100

    added update endpoint

--- main.go
+++ main.go
@@ -26,9 +26,40 @@ func init() {
 
 func main() {
 	router := gin.Default()
-	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.POST("/recipes", NewRecipeHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	idx := -1
+	for i, r := range recipes {
+		if r.ID == id {
+			recipe.ID = r.ID
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "no records exist for that id",
+		})
+		return
+	}
+
+	recipes[idx] = recipe
+	c.JSON(http.StatusAccepted, recipe)
 }
 
 func ListRecipesHandler(c *gin.Context) {
@@ -46,7 +77,7 @@ func NewRecipeHandler(c *gin.Context) {
 	recipe.ID = uuid.NewString()
 	recipe.PublishedAt = time.Now()
 	recipes = append(recipes, recipe)
-	c.JSON(http.StatusOK, recipe)
+	c.JSON(http.StatusCreated, recipe)
 }
 
 type Recipe struct {
